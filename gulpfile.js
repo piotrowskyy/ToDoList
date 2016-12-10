cb124ede880d008645f0c40f3d182ab298ef5b1a
Pawel Bujna <pawelbujna@Pawels-MacBook-Air.local>
2016-12-10 15:41:59 +0100

    Added new scss navbar and settings file

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,21 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var server = require('gulp-server-livereload');
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
 	gulp.src('app/scss/style.scss')
-	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest('app/css'));
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('app/css'));
 });
 
-gulp.task('watch-sass', function() {
-	gulp.watch('app/scss/*.scss', ['styles']);
+gulp.task('watch-sass', function () {
+	gulp.watch('app/scss/**/*.scss', ['styles']);
 });
 
-gulp.task('webserver', ['watch-sass'], function() {
+gulp.task('webserver', ['watch-sass'], function () {
 	gulp.src('app')
-	.pipe(server({
-		defaultFile: 'index.html',
-		livereload: true,
-		open: true,
-	}));
+		.pipe(server({
+			defaultFile: 'index.html',
+			livereload: true,
+			open: true,
+		}));
 });
